3d788555c2db3180a02f3c3a49f231c8b64e078f
inti0291 <inti0291@DESKTOP-4PAPS7N>
2016-07-01 11:33:02 +0200

    commit 01/07/16 BanqueDaoImp, IBanqueDao, ClientDaoImp, IClientDao,
    ExceptionAucunClientTrouve, ExceptionAucunCompteTrouveDepuisClient

--- ProjectGestionBaqnques/src/com/groupe1/adaming/daoBanque/IBanqueDao.java
+++ ProjectGestionBaqnques/src/com/groupe1/adaming/daoBanque/IBanqueDao.java
@@ -1,5 +1,17 @@
 package com.groupe1.adaming.daoBanque;
+/*Class: IBanqueDao
+ * Interface DAO Banque
+ * Package: com.groupe1.adaming.daoBanque
+ * Author: Philippe
+ * Version: 1.0.0
+ * Date: 01/07/16
+ */
+import java.util.Collection;
+
+import com.groupe1.adaming.entities.Banque;
 
 public interface IBanqueDao {
-
+	/* methodes implementees dans BanqueDaoImp */
+	public Banque addBanque (Banque banque);
+	public Collection<Banque> getBanques();
 }
--- ProjectGestionBaqnques/src/com/groupe1/adaming/exception/ExceptionAucunClientTrouve.java
+++ ProjectGestionBaqnques/src/com/groupe1/adaming/exception/ExceptionAucunClientTrouve.java
@@ -1,5 +1,18 @@
 package com.groupe1.adaming.exception;
 
-public class ExceptionAucunClientTrouve {
+/*Class: ExceptionAucunClientTrouve
+ * Exception personnalisee pour la methode public Collection<Client> getClientParMC(String mc) de la DAO client
+ * Package: com.groupe1.adaming.exception
+ * Author: Philippe
+ * Version: 1.0.0
+ * Date: 01/07/16
+ */
+@SuppressWarnings("serial")
+public class ExceptionAucunClientTrouve extends Exception {
+
+	public ExceptionAucunClientTrouve(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
 
 }

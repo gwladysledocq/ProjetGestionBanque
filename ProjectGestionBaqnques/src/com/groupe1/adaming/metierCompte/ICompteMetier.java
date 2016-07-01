5c139776924ca132fbb8bb957e649d2cfe4d5775
inti0300 <inti0300@DESKTOP-VATSLPR>
2016-07-01 11:54:11 +0200

    Metier Employe et Compte

--- ProjectGestionBaqnques/src/com/groupe1/adaming/metierCompte/ICompteMetier.java
+++ ProjectGestionBaqnques/src/com/groupe1/adaming/metierCompte/ICompteMetier.java
@@ -1,5 +1,24 @@
 package com.groupe1.adaming.metierCompte;
 
+import java.util.Collection;
+
+import com.groupe1.adaming.entities.Compte;
+/*
+ * Classe: ICompteMetier
+ * Package: com.groupe1.adaming.metierCompte
+ * Description: Interface
+ * Auteur: Romain Ganet
+ * Version: 1.0.0
+ * Date: 31/06/2016
+ * 
+ */
+
 public interface ICompteMetier {
+	
+	public Compte addCompte(Compte compte, Long idClient, Long idEmploye);
+	public Compte deleteCompte(Long idCompte);
+	public Collection<Compte> getComptesParClient(Compte compte, Long idClient);
+	public Collection<Compte> getComptesParEmploye(Compte compte, Long idEmploye);
+	public Compte updateCompte(Compte compte);
 
 }

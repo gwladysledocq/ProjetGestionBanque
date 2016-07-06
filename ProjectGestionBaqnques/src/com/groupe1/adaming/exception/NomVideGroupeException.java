c6ac991dc4667966e1b66f991bb72407f42b1289
inti0215 <inti0215@INTI-0215>
2016-07-06 12:39:19 +0200

    Commit Exception

---
+++ ProjectGestionBaqnques/src/com/groupe1/adaming/exception/NomVideGroupeException.java
@@ -0,0 +1,13 @@
+package com.groupe1.adaming.exception;
+
+@SuppressWarnings("serial")
+public class NomVideGroupeException extends Exception{
+
+	public NomVideGroupeException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
